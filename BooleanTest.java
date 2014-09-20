266a0b3eee4c2a10694ee42df326f6b2b33dea08
SoftwareA6 <763300586@qq.com>
2014-09-21 00:28:27 +0800

    add BooleanTest.java

---
+++ BooleanTest.java
@@ -0,0 +1,14 @@
+public class BooleanTest{
+	static boolean isA;
+	static boolean isB = true;
+	public static void main(String[] args){
+		System.out.println("isA is : "+isA);
+		System.out.println("isB is : "+isB);
+		if(isA){
+			System.out.println("isA is true");
+		}
+		if(isB){
+			System.out.println("isB is true");
+		}
+		}
+}
